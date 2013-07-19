9ee61a4c1e242917c0bb8dfc07d9523bdb9f8d60
Abelardo Lopez-Lagunas <abelardo.lopez@gmail.com>
2013-07-18 21:58:10 -0500

    Changed web.js and index.html so that it works from a remote web server

--- web.js
+++ web.js
@@ -3,12 +3,18 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+// Use the root and the /media directory, later on
+// add a css and js directories to leave the html alone
+// However. I'll need to check if this impacts performance
+app.use(app.router);
+app.use(express.static(__dirname + '/media/'));
+
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
     response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
